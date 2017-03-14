3164844d272815aa466ad1bb4bf7ea339206ccd6
GRUCHET Sebastien <sgruchet@ipb.fr>
2017-03-14 10:58:54 +0100

    script

---
+++ colorization/exec.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+./colorization ../data/sat-c.ppm ../data/sat-g.ppm a.ppm
+./colorization ../data/sat-c.ppm ../data/sat-g.ppm b.ppm
+./colorization ../data/sat-c.ppm ../data/sat-g.ppm c.ppm
+./colorization ../data/sat-c.ppm ../data/sat-g.ppm d.ppm
+./colorization ../data/sat-c.ppm ../data/sat-g.ppm e.ppm
+./colorization ../data/sat-c.ppm ../data/sat-g.ppm f.ppm
+./colorization ../data/sat-c.ppm ../data/sat-g.ppm g.ppm
+./colorization ../data/sat-c.ppm ../data/sat-g.ppm h.ppm
+./colorization ../data/sat-c.ppm ../data/sat-g.ppm i.ppm
+./colorization ../data/sat-c.ppm ../data/sat-g.ppm j.ppm
+./colorization ../data/sat-c.ppm ../data/sat-g.ppm k.ppm
+
+
+exit 0
